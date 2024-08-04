fcc2e1514b8622b5e64c9910ab58bad42e8d68b3
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-04 18:13:48 -0500

    chore: Add new home page with sections and components

---
+++ src/app/(home)/home-4/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import SectionHero from "@/app/(server-components)/SectionHero";
+import BgGlassmorphism from "@/components/BgGlassmorphism";
+import SectionOurFeatures from "@/components/SectionOurFeatures";
+import SectionHowItWork from "@/components/SectionHowItWork";
+import SectionBecomeAnAuthor from "@/components/SectionBecomeAnAuthor";
+import SectionVideos from "@/components/SectionVideos";
+
+function PageHome() {
+  return (
+    <main className="nc-PageHome relative overflow-hidden">
+      {/* GLASSMOPHIN */}
+      <BgGlassmorphism />
+
+      <div className="container relative space-y-24 mb-24 lg:space-y-28 lg:mb-28">
+        {/* SECTION HERO */}
+        <SectionHero className="pt-10 lg:pt-16 lg:pb-16" />
+
+        <SectionOurFeatures />
+
+        <SectionHowItWork />
+
+        <SectionVideos />
+
+        <SectionBecomeAnAuthor />
+      </div>
+    </main>
+  );
+}
+
+export default PageHome;
