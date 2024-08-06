6e33a395afcf2bc50aa64fab02de13d345179fc6
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-06 16:15:20 -0500

    chore: Update MainNav1 component to include SwitchDarkMode in hidden md:flex

--- src/app/(client-components)/(Header)/MainNav1.tsx
+++ src/app/(client-components)/(Header)/MainNav1.tsx
@@ -30,9 +30,11 @@ const MainNav1: FC<MainNav1Props> = ({ className = "" }) => {
 
         <div className="flex flex-shrink-0 justify-end flex-1 lg:flex-none text-neutral-700 dark:text-neutral-100">
           <div className="flex space-x-0.5">
-            <SwitchDarkMode />
             {/* <SearchDropdown className="flex items-center" /> */}
-            <div className="px-1" />
+            <div className="hidden md:flex">
+              <SwitchDarkMode />
+              <div className="px-1" />
+            </div>
             <ButtonPrimary className="self-center" href="/login">
               Sign In
             </ButtonPrimary>
