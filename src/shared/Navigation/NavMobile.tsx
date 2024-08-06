d70edd63198d4c6b3b7294f35200a6b0a6bf1ffe
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-06 17:18:37 -0500

    chore: Update NavMobile to reflect job search focus

--- src/shared/Navigation/NavMobile.tsx
+++ src/shared/Navigation/NavMobile.tsx
@@ -109,8 +109,8 @@ const NavMobile: React.FC<NavMobileProps> = ({
         <Logo />
         <div className="flex flex-col mt-5 text-neutral-700 dark:text-neutral-300 text-sm">
           <span>
-            Discover the most outstanding articles on all topics of life. Write
-            your stories and share them
+            Discover the most outstanding job opportunities in the world. We are
+            here to help you find the best job for you.
           </span>
 
           <div className="flex justify-between items-center mt-4">
