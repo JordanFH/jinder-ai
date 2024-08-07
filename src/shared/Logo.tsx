d2adca6790218f5122838b04c3a8117f3fd317db
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-07 09:00:03 -0500

    chore: Update Logo component usage in MainNav1 and SectionBecomeAnAuthor

--- src/shared/Logo.tsx
+++ src/shared/Logo.tsx
@@ -15,7 +15,7 @@ export interface LogoProps {
 const Logo: React.FC<LogoProps> = ({
   img = logoImg,
   imgLight = logoLightImg,
-  className = "w-24",
+  className = "",
 }) => {
   return (
     <Link
