ef8e75d6e4ca5d161b54c23e772eeca976cd251a
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-07 11:56:54 -0500

    chore: Update SectionHero component layout and styles

--- src/app/(server-components)/SectionHero.tsx
+++ src/app/(server-components)/SectionHero.tsx
@@ -14,7 +14,7 @@ const SectionHero: FC<SectionHeroProps> = ({ className = "" }) => {
       className={`nc-SectionHero flex flex-col-reverse lg:flex-col relative ${className}`}
     >
       <div className="flex flex-col lg:flex-row lg:items-center">
-        <div className="flex-shrink-0 lg:w-1/2 flex flex-col items-start space-y-8 sm:space-y-10 pb-14 lg:pb-64 xl:pr-14 lg:mr-10 xl:mr-0">
+        <div className="flex-shrink-0 lg:w-1/2 flex flex-col items-start space-y-8 sm:space-y-10 pb-10 lg:pb-14 xl:pr-14 lg:mr-10 xl:mr-0">
           <h2 className="font-medium text-4xl md:text-5xl xl:text-7xl !leading-[114%] ">
             Hotel, car & experiences
           </h2>
@@ -29,10 +29,6 @@ const SectionHero: FC<SectionHeroProps> = ({ className = "" }) => {
         <div className="flex-grow">
           <Image className="w-full" src={imagePng} alt="hero" priority />
         </div>
-      </div>
-
-      <div className="hidden lg:block z-10 mb-12 lg:mb-0 lg:-mt-40 w-full">
-        <HeroSearchForm />
       </div>
     </div>
   );
