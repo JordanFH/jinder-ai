07a30be0e6a1078e6bff2ff855e4687cd60d075c
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-08 12:53:05 -0500

    chore: Remove unused Firebase-related code in auth route file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,4 @@
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-import { cert } from "firebase-admin/app";
 import NextAuth from "next-auth";
-import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
@@ -11,13 +8,6 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
-  adapter: FirestoreAdapter({
-    credential: cert({
-      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-    }),
-  }) as Adapter,
   pages: {
     signIn: "/login",
   },
