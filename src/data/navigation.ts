1e0dae3557e4a6e51aecf2f86551ad6417b137c4
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-12 00:33:46 -0500

    refactor: Remove unused car listing pages and components

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -229,19 +229,6 @@ export const NAVIGATION_DEMO: NavItemType[] = [
       //
       {
         id: ncNanoId(),
-        href: "/listing-car",
-        name: "Cars listings",
-        type: "dropdown",
-        children: [
-          { id: ncNanoId(), href: "/listing-car", name: "Cars page" },
-          { id: ncNanoId(), href: "/listing-car-map", name: "Cars page (map)" },
-          { id: ncNanoId(), href: "/listing-car-detail", name: "Car Detail" },
-        ],
-      },
-
-      //
-      {
-        id: ncNanoId(),
         href: "/listing-real-estate",
         name: "Real Estate Listings",
         type: "dropdown",
@@ -337,34 +324,6 @@ export const NAVIGATION_DEMO_2: NavItemType[] = [
         id: ncNanoId(),
         href: "/listing-experiences-detail",
         name: "Experiences detail",
-      },
-    ],
-  },
-  {
-    id: ncNanoId(),
-    href: "/listing-car",
-    name: "Listing pages",
-    children: [
-      { id: ncNanoId(), href: "/listing-car", name: "Cars listings" },
-      { id: ncNanoId(), href: "/listing-car-map", name: "Cars listings (map)" },
-      { id: ncNanoId(), href: "/listing-car-detail", name: "Car detail" },
-
-      //
-      {
-        id: ncNanoId(),
-        href: "/listing-real-estate",
-        name: "Real estate listings",
-      },
-      {
-        id: ncNanoId(),
-        href: "/listing-real-estate-map",
-        name: "Real estate (map)",
-      },
-      //
-      {
-        id: ncNanoId(),
-        href: "/listing-flights",
-        name: "Flights listings",
       },
     ],
   },
