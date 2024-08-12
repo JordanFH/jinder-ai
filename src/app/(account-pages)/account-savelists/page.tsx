ee5802670afa7a5c1dea788bc3305684fa9ac39a
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-11 23:53:14 -0500

    refactor: Improve saved item handling in SectionCourses and SectionJobs components

--- src/app/(account-pages)/account-savelists/page.tsx
+++ src/app/(account-pages)/account-savelists/page.tsx
@@ -2,15 +2,182 @@
 
 import { Tab } from "@headlessui/react";
 import CarCard from "@/components/CarCard";
-import {
-  DEMO_CAR_LISTINGS,
-  DEMO_STAY_LISTINGS,
-} from "@/data/listings";
-import React, { Fragment, useState } from "react";
-import PropertyCardH from "@/components/PropertyCardH";
+import React, { Fragment, useEffect, useState } from "react";
+import { useUser } from "@/providers/UserProvider";
+import { getUserByEmail, updateUserByEmail } from "@/utils/userUtils";
+import toast from "react-hot-toast";
 
 const AccountSavelists = () => {
+  const user = useUser();
   let [categories] = useState(["Jobs", "Courses"]);
+
+  // COURSES
+  const [savedCourses, setSavedCourses] = useState<any>(null);
+
+  useEffect(() => {
+    if (!savedCourses) {
+      getUserByEmail(user.email).then((user) => {
+        if (user) {
+          const { courses } = user.preferences.saved;
+          setSavedCourses(courses);
+        }
+      });
+    }
+  }, [user, savedCourses]);
+
+  const handleSaveCourse = (data: any) => {
+    if (savedCourses) {
+      getUserByEmail(user.email).then((user) => {
+        if (user) {
+          toast.loading("Saving...");
+          const newSaved = [...savedCourses, data];
+
+          const updatedUser = {
+            ...user,
+            preferences: {
+              ...user.preferences,
+              saved: {
+                ...user.preferences.saved,
+                courses: newSaved,
+              },
+            },
+          };
+
+          updateUserByEmail(user.email, updatedUser)
+            .then(() => {
+              setSavedCourses(newSaved);
+              toast.dismiss();
+              toast.success("Saved successfully!");
+            })
+            .catch((error) => {
+              console.error("Error updating document: ", error);
+              toast.dismiss();
+              toast.error("Failed to save");
+            });
+        }
+      });
+    }
+  };
+
+  const handleRemoveCourse = (data: any) => {
+    if (savedCourses) {
+      getUserByEmail(user.email).then((user) => {
+        if (user) {
+          toast.loading("Removing...");
+          const newSaved = savedCourses.filter(
+            (item: any) => item.title !== data.title
+          );
+
+          const updatedUser = {
+            ...user,
+            preferences: {
+              ...user.preferences,
+              saved: {
+                ...user.preferences.saved,
+                courses: newSaved,
+              },
+            },
+          };
+
+          updateUserByEmail(user.email, updatedUser)
+            .then(() => {
+              setSavedCourses(newSaved);
+              toast.dismiss();
+              toast.success("Removed successfully!");
+            })
+            .catch((error) => {
+              console.error("Error updating document: ", error);
+              toast.dismiss();
+              toast.error("Failed to remove");
+            });
+        }
+      });
+    }
+  };
+
+  // JOBS
+  const [savedJobs, setSavedJobs] = useState<any>(null);
+
+  useEffect(() => {
+    if (!savedJobs) {
+      getUserByEmail(user.email).then((user) => {
+        if (user) {
+          const { jobs } = user.preferences.saved;
+          setSavedJobs(jobs);
+        }
+      });
+    }
+  }, [user, savedJobs]);
+
+  const handleSaveJob = (data: any) => {
+    if (savedJobs) {
+      getUserByEmail(user.email).then((user) => {
+        if (user) {
+          toast.loading("Saving...");
+          const newSaved = [...savedJobs, data];
+
+          const updatedUser = {
+            ...user,
+            preferences: {
+              ...user.preferences,
+              saved: {
+                ...user.preferences.saved,
+                jobs: newSaved,
+              },
+            },
+          };
+
+          updateUserByEmail(user.email, updatedUser)
+            .then(() => {
+              setSavedJobs(newSaved);
+              toast.dismiss();
+              toast.success("Saved successfully!");
+            })
+            .catch((error) => {
+              console.error("Error updating document: ", error);
+              toast.dismiss();
+              toast.error("Failed to save");
+            });
+        }
+      });
+    }
+  };
+
+  const handleRemoveJob = (data: any) => {
+    if (savedJobs) {
+      getUserByEmail(user.email).then((user) => {
+        if (user) {
+          toast.loading("Removing...");
+          const newSaved = savedJobs.filter(
+            (item: any) => item.title !== data.title
+          );
+
+          const updatedUser = {
+            ...user,
+            preferences: {
+              ...user.preferences,
+              saved: {
+                ...user.preferences.saved,
+                jobs: newSaved,
+              },
+            },
+          };
+
+          updateUserByEmail(user.email, updatedUser)
+            .then(() => {
+              setSavedJobs(newSaved);
+              toast.dismiss();
+              toast.success("Removed successfully!");
+            })
+            .catch((error) => {
+              console.error("Error updating document: ", error);
+              toast.dismiss();
+              toast.error("Failed to remove");
+            });
+        }
+      });
+    }
+  };
 
   const renderSection1 = () => {
     return (
@@ -41,16 +208,28 @@ const AccountSavelists = () => {
             </Tab.List>
             <Tab.Panels>
               <Tab.Panel className="mt-8">
-                <div className="grid grid-cols-1 gap-6 md:gap-8 xl:grid-cols-2">
-                  {DEMO_STAY_LISTINGS.filter((_, i) => i < 8).map((stay) => (
-                    <PropertyCardH key={stay.id} data={stay} />
+                <div className="grid grid-cols-1 gap-6 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                  {savedJobs?.map((job: any) => (
+                    <CarCard
+                      key={job.id}
+                      data={job}
+                      isLiked={true}
+                      handleSave={handleSaveJob}
+                      handleRemove={handleRemoveJob}
+                    />
                   ))}
                 </div>
               </Tab.Panel>
               <Tab.Panel className="mt-8">
                 <div className="grid grid-cols-1 gap-6 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                  {DEMO_CAR_LISTINGS.filter((_, i) => i < 8).map((stay) => (
-                    <CarCard key={stay.id} data={stay} />
+                  {savedCourses?.map((course: any) => (
+                    <CarCard
+                      key={course.id}
+                      data={course}
+                      isLiked={true}
+                      handleSave={handleSaveCourse}
+                      handleRemove={handleRemoveCourse}
+                    />
                   ))}
                 </div>
               </Tab.Panel>
