578a4cd463d80deabd9311604a32a88b4856c39a
JordanFH <jfernandezh19_2@unc.edu.pe>
2024-08-11 19:35:36 -0500

    refactor: Remove commented out code in LoginForm component

--- src/app/login/(componentes)/LoginForm.tsx
+++ src/app/login/(componentes)/LoginForm.tsx
@@ -34,46 +34,6 @@ export const LoginForm = () => {
           Login
         </h2>
         <div className="max-w-md mx-auto space-y-6">
-          {/* FORM */}
-          {/* <form className="grid grid-cols-1 gap-6" action="#" method="post">
-            <label className="block">
-              <span className="text-neutral-800 dark:text-neutral-200">
-                Email address
-              </span>
-              <Input
-                type="email"
-                placeholder="example@example.com"
-                className="mt-1"
-              />
-            </label>
-            <label className="block">
-              <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">
-                Password
-                <Link href={{ pathname: "/login" }} className="text-sm underline font-medium">
-                  Forgot password?
-                </Link>
-              </span>
-              <Input type="password" className="mt-1" />
-            </label>
-            <ButtonPrimary type="submit">Continue</ButtonPrimary>
-          </form> */}
-
-          {/* ==== */}
-          {/* <span className="block text-center text-neutral-700 dark:text-neutral-300">
-            New user? {` `}
-            <Link href={{ pathname: "/signup" }} className="font-semibold underline">
-              Create an account
-            </Link>
-          </span> */}
-
-          {/* OR */}
-          {/* <div className="relative text-center">
-            <span className="relative z-10 inline-block px-4 font-medium text-sm bg-white dark:text-neutral-400 dark:bg-neutral-900">
-              OR
-            </span>
-            <div className="absolute left-0 w-full top-1/2 transform -translate-y-1/2 border border-neutral-100 dark:border-neutral-800"></div>
-          </div> */}
-
           {/* SOCIALS */}
           <div className="grid gap-3">
             {loginSocials.map((item, index) => (
